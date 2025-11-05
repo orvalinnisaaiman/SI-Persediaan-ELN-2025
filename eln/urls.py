4942313a33ae054be8b1586a478bc8a16cfbae1f
Orva Linnisa <orvalinnisa@student.ub.ac.id>
2025-11-05 15:37:13 +0700

    Fix root redirect to avoid NoReverseMatch

--- eln/urls.py
+++ eln/urls.py
@@ -1,28 +1,19 @@
-"""
-URL configuration for eln project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse, NoReverseMatch
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    # urutan prioritas: ganti sesuai kebiasaanmu
+    for name in ('base', 'login'):
+        try:
+            return redirect(reverse(name))
+        except NoReverseMatch:
+            pass
+    # fallback terakhir kalau nama URL di atas nggak ada
+    return redirect('/accounts/login/')
 
 urlpatterns = [
-        path('', RedirectView.as_view(pattern_name='login', permanent=False)),
+    path('', root_redirect, name='root'),
+    path('', include('inventory.urls')),   # sesuaikan app utama
     path('admin/', admin.site.urls),
-    path('', include('inventory.urls'))
 ]
-
-from django.views.generic import RedirectView
-
-
